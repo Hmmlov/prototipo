9cf34df2a831a575da66960fcac2c0ddd4ed04db
miguel <miguelcondegarcia26@gmail.com>
2022-10-07 20:12:24 -0500

    cambioapp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
-import Inicio from './Views/Inicio';
-import Menu from './Views/Menu';
-
-
-import './App.css';
-import Inicio from './Views/Inicio'
-import Menu from './Views/Menu'
+import Inicio from './views/Inicio'
+import Menu from './views/Menu'
 
 function App() {
   return (
